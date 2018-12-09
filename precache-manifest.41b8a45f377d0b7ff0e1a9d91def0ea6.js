1d01214aeccfa350277030a49d0fc90aaf04bb61
Delphine <del.millet@gmail.com>
2018-12-09 17:55:01 +0100

    Updates

---
+++ precache-manifest.41b8a45f377d0b7ff0e1a9d91def0ea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a34bd251c37b6c4e37fac91be07b80e1",
+    "url": "/manage-users/static/media/logo.a34bd251.png"
+  },
+  {
+    "revision": "2c6c409730bd7a401b3c",
+    "url": "/manage-users/static/js/runtime~main.2c6c4097.js"
+  },
+  {
+    "revision": "78fffeac73d87d82e2c4",
+    "url": "/manage-users/static/js/main.78fffeac.chunk.js"
+  },
+  {
+    "revision": "7b5d9ed5bead3834cee3",
+    "url": "/manage-users/static/js/1.7b5d9ed5.chunk.js"
+  },
+  {
+    "revision": "78fffeac73d87d82e2c4",
+    "url": "/manage-users/static/css/main.32b7699d.chunk.css"
+  },
+  {
+    "revision": "5938506bcc1de27675b6cc29cea5ff33",
+    "url": "/manage-users/index.html"
+  }
+];
